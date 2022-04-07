4a0cade491d4bcc1650dcc1b53ce12481188ef21
Flávia Souza <88092488+flaviasouzza@users.noreply.github.com>
2022-04-07 12:11:08 -0300

    Add files via upload

---
+++ SalaryPercentage.java
@@ -0,0 +1,13 @@
+package Logicadeprogramacao;
+
+public class SalaryPercentage {
+    public static void main (String [] args){
+
+        float salary = 1500.0F;
+        float percentage = 35;
+        float resultepercentage = salary * percentage / 100;
+
+        System.out.println("The percentage of " + percentage + "%" + " of the salary is " + resultepercentage);
+
+    }
+}
